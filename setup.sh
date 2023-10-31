6208a43f6a055ff963ba02f3fa538e0dececbfd2
Thilo Rusche <thilorusche@gmail.com>
2023-10-31 15:07:58 +0000

    auto setup

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+ln -s ~/dotfiles/bash_profile ~/.bash_profile
+ln -s ~/dotfiles/gitconfig ~/.gitconfig
+ln -s ~/dotfiles/aliasrc ~/.aliasrc
+ln -s ~/dotfiles/gemrc ~/.gemrc
+ln -s ~/dotfiles/irbrc ~/.irbrc
+ln -s ~/dotfiles/vimrc ~/.vimrc
